7278880a8c662d67adea431db03134ef9a44a102
Illya <i.panyushkin@keepsolid.com>
2021-10-27 01:39:50 +0300

    add-data field

---
+++ src/components/dataTable.vue
@@ -0,0 +1,12 @@
+<template>
+  <tr class="data">
+    <slot name="id"></slot>
+    <slot name="name"></slot>
+    <slot name="surname"></slot>
+    <slot name="date"></slot>
+
+  </tr>
+</template>
+
+<style lang="scss" scoped>
+</style>
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,4 @@
-export default function firstLetterToUpperCase(item) {
+export const firstLetterToUpperCase = (item) => {
     return item.charAt(0).toUpperCase() + item.slice(1);
+
 }
